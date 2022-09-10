4cc7d7b8df8bf72a153e572a8f9da0d0ac21bd01
sansD92 <adjiedwisandy@gmail.com>
2022-09-11 01:04:16 +0700

    a

---
+++ application/views/frontends/header.php
@@ -0,0 +1,53 @@
+<header class="header header-four">
+<nav class="navbar navbar-expand-lg header-nav">
+<div class="container">
+<div class="navbar-header">
+<a id="mobile_btn" href="javascript:void(0);">
+<span class="bar-icon">
+<span></span>
+<span></span>
+<span></span>
+</span>
+</a>
+<a href="index.html" class="navbar-brand logo">
+<img src="<?php echo base_url()?>assets/frontends/img/logo-four.png" class="img-fluid" alt="Logo">
+</a>
+</div>
+<div class="main-menu-wrapper">
+<div class="menu-header">
+<a href="index.html" class="menu-logo">
+<img src="<?php echo base_url()?>assets/frontends/img/logo-four.png" class="img-fluid" alt="Logo">
+</a>
+<a id="menu_close" class="menu-close" href="javascript:void(0);">
+<i class="fas fa-times"></i>
+</a>
+</div>
+<ul class="main-nav">
+  <li>
+  <a href="#" target="_blank">Beranda</a>
+  </li>
+  <li>
+  <a href="#" target="_blank">Tentang Kami</a>
+  </li>
+  <li>
+  <a href="#" target="_blank">Layanan</a>
+  </li>
+  <li>
+  <a href="#" target="_blank">Bantuan</a>
+  </li>
+  <li>
+  <a href="#" target="_blank">Artikel</a>
+  </li>
+</ul>
+</div>
+<ul class="nav header-navbar-rht sign-page">
+<li><a href="register.html" class="reg-btn"> Masuk</a></li>
+<li><img src="<?php echo base_url()?>assets/frontends/img/key-01.png" alt="Logo"></li>
+<li><a href="login.html" class="log-btn"> Daftar</a></li>
+</ul>
+<ul class="nav header-navbar-rht text-center">
+<li><a href="post-project.html" class="btn login-btn">Bahasa </a></li>
+</ul>
+</div>
+</nav>
+</header>
