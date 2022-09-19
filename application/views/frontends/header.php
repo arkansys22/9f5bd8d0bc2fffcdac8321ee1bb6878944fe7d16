8cf3e82e176dbe61d6714d50aa1b67dec2138b00
tempebener <bayoewa@gmail.com>
2022-09-20 06:05:16 +0700

    Update header.php

--- application/views/frontends/header.php
+++ application/views/frontends/header.php
@@ -24,7 +24,7 @@
 </div>
 <ul class="main-nav">
   <li>
-  <a href="#" target="_blank">Beranda</a>
+  <a href="<?php echo base_url()?>" target="_blank">Beranda</a>
   </li>
   <li>
   <a href="#" target="_blank">Tentang Kami</a>
