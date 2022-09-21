c58d540c2c6fa18848540877b9687f43c667f9e9
sansD92 <adjiedwisandy@gmail.com>
2022-09-21 16:28:24 +0700

    up

--- application/views/frontends/layanan/konsultasi_online/index.php
+++ application/views/frontends/layanan/konsultasi_online/index.php
@@ -37,7 +37,147 @@
 <div class="container">
 <div class="row">
 <div class="col-12">
-<h5>Blank Page</h5>
+<div class="my-projects-view favourite-group">
+<div class="row">
+<div class="col-lg-12">
+<div class="card">
+<div class="card-header d-flex">
+<h5 class="card-title">Bookmarked Projects</h5>
+<ul class="list-grid d-flex align-items-center">
+<li><a href="freelancer-favourites.html"><i class="fas fa-list"></i></a></li>
+<li><a href="freelancer-favourites.html" class="favour-active"><i class="fas fa-th-large"></i></a></li>
+</ul>
+</div>
+<div class="freelance-box book-mark bookmark-projects">
+<div class="row">
+<div class="col-md-12 col-lg-12 col-xl-12">
+<div class="relavance-result d-flex align-items-center">
+<div class="relavance-rate d-flex justify-content-sm-end">
+<!-- <div class="sort-by">
+<label>Sort By: </label>
+<select class="custom-select form-select">
+<option>Relevance</option>
+<option>Rating</option>
+<option>Popular</option>
+<option>Latest</option>
+<option>Free</option>
+</select>
+</div> -->
+</div>
+</div>
+</div>
+
+<div class="col-md-6 col-lg-12 col-xl-3">
+<div class="freelance-widget widget-author">
+<div class="freelance-content">
+<a data-bs-toggle="modal" href="#rating" class="favourite favourited"><i class="fas fa-star"></i></a>
+<div class="author-heading">
+<div class="profile-img">
+<a href="project-details.html">
+<img src="<?php echo base_url()?>assets/frontends/img/company/img-1.png" alt="author">
+<span class="project-verified"><i class="fas fa-check-circle"></i></span>
+</a>
+</div>
+<div class="profile-name">
+<div class="author-location"><a href="project-details.html">Amaze Tech </a></div>
+</div>
+
+
+<div class="freelancers-price"></div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="col-md-6 col-lg-12 col-xl-3">
+<div class="freelance-widget widget-author">
+<div class="freelance-content">
+<a data-bs-toggle="modal" href="#rating" class="favourite favourited"><i class="fas fa-star"></i></a>
+<div class="author-heading">
+<div class="profile-img">
+<a href="project-details.html">
+<img src="<?php echo base_url()?>assets/frontends/img/company/img-2.png" alt="author">
+<span class="project-verified"><i class="fas fa-check-circle"></i></span>
+</a>
+</div>
+<div class="profile-name">
+<div class="author-location"><a href="project-details.html">Park INC </a></div>
+</div>
+
+
+<div class="freelancers-price"></div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+<div class="col-md-6 col-lg-12 col-xl-3">
+<div class="freelance-widget widget-author">
+<div class="freelance-content">
+<a data-bs-toggle="modal" href="#rating" class="favourite favourited"><i class="fas fa-star"></i></a>
+<div class="author-heading">
+<div class="profile-img">
+<a href="project-details.html">
+<img src="<?php echo base_url()?>assets/frontends/img/company/img-1.png" alt="author">
+<span class="project-verified"><i class="fas fa-check-circle"></i></span>
+</a>
+</div>
+<div class="profile-name">
+<div class="author-location"><a href="project-details.html">Amaze Tech </a></div>
+</div>
+
+
+<div class="freelancers-price"></div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+<div class="col-md-6 col-lg-12 col-xl-3">
+<div class="freelance-widget widget-author">
+<div class="freelance-content">
+<a data-bs-toggle="modal" href="#rating" class="favourite favourited"><i class="fas fa-star"></i></a>
+<div class="author-heading">
+<div class="profile-img">
+<a href="project-details.html">
+<img src="<?php echo base_url()?>assets/frontends/img/company/img-1.png" alt="author">
+<span class="project-verified"><i class="fas fa-check-circle"></i></span>
+</a>
+</div>
+<div class="profile-name">
+<div class="author-location"><a href="project-details.html">Amaze Tech </a></div>
+</div>
+
+
+<div class="freelancers-price"></div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
 </div>
 </div>
 </div>
